f6b57a498db2cf6d96fa85b15888294a61b57924
Ruth <easerratic21>
2025-04-01 08:44:06 -0300

    puntero en tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    int variable = 8;         
+    int *puntero = &variable; 
+
+    printf("1) Contenido del puntero: %d\n", *puntero);
+
+    printf("2) Direccion almacenada por el puntero: %p\n", (void*)puntero);
+
+    printf("3) Direccion de la variable: %p\n", (void*)&variable);
+
+    printf("4) Direccion del puntero: %p\n", (void*)&puntero);
+
+    printf("5) Tamanio de la variable: %zu bytes\n", sizeof(variable));
+
     return 0;
 }
