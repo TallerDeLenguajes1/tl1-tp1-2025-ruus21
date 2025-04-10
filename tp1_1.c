725b34b6c38c5a50afab7dcceea7d6d0bb4f59ba
Ruth <easerratic21>
2025-04-10 19:57:45 -0300

    punto 1 arreglado

--- tp1_1.c
+++ tp1_1.c
@@ -6,11 +6,11 @@ int main(){
 
     printf("1) Contenido del puntero: %d\n", *puntero);
 
-    printf("2) Direccion almacenada por el puntero: %p\n", (void*)puntero);
+    printf("2) Direccion almacenada por el puntero: %p\n", puntero);
 
-    printf("3) Direccion de la variable: %p\n", (void*)&variable);
+    printf("3) Direccion de la variable: %p\n", &variable);
 
-    printf("4) Direccion del puntero: %p\n", (void*)&puntero);
+    printf("4) Direccion del puntero: %p\n", &puntero);
 
     printf("5) Tamanio de la variable: %zu bytes\n", sizeof(variable));
 
